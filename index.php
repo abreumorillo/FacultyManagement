c2957a763e4a997739ff42ac08048256314b86eb
Neris S. Abreu <abreumorillo@gmail.com>
2015-10-21 11:58:56 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+    require_once('vendor/autoload.php');
+    echo "Today is " . date("Y/m/d") . "<br>";
+    echo "Today is " . date("Y.m.d") . "<br>";
+    echo "Today is " . date("Y-m-d") . "<br>";
+    echo "Today is " . date("l");
+    
+    echo "<br>";
+    echo getcwd() . "\n";
+
+?>
---
+++ readme.md
@@ -0,0 +1,4 @@
+#<h1 style="text-aligh:center;">ISTE-330 Database Connectivity and Access</h1>
+##Faculty Research Database
+<br>
+###Documentation...
